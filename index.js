7e139d30a16c427a043c0a7dc118f56df5f6c9fb
Seun-O <12373359+Seun-O@users.noreply.github.com>
2019-05-27 19:09:48 -0400

    MVP Complete

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const helmet = require("helmet");
+const db = require("./knex");
 
 const server = express();
 
@@ -7,6 +8,65 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.get("/api/zoos", async (req, res) => {
+  try {
+    const data = await db.find();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ err, message: "Internal Server Error" });
+  }
+});
+
+server.get("/api/zoos/:id", async (req, res) => {
+  try {
+    const data = await db.findById(req.params.id);
+    if (!data) {
+      res.status(404).json({ message: "ID not found" });
+    }
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ err, message: "Internal Server Error" });
+  }
+});
+
+server.delete("/api/zoos/:id", async (req, res) => {
+  try {
+    const data = await db.remove(req.params.id);
+
+    if (!data) {
+      res.status(404).json({ message: "ID not found" });
+    }
+    res.status(204).json(data);
+  } catch (err) {
+    res.status(500).json({ err, message: "Internal Server Error" });
+  }
+});
+
+server.post("/api/zoos/", async (req, res) => {
+  try {
+    if (Object.keys(req.body).length === 0) {
+      res.status(400).json({ message: "Please Include a request body." });
+    } else if (!req.body.name) {
+      res.status(400).json({ message: "Name field is required in the body" });
+    }
+    const data = await db.add(req.body);
+    res.status(201).json(data);
+  } catch (err) {
+    res.status(500).json({ err, message: "Internal Server Error" });
+  }
+});
+server.put("/api/zoos/:id", async (req, res) => {
+  try {
+    const data = await db.update(req.params.id, req.body);
+    if (!data) {
+      res.status(404).json({ message: "ID not found" });
+    }
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ err, message: "Internal Server Error" });
+  }
+});
 
 const PORT = 3300;
 server.listen(PORT, function() {
